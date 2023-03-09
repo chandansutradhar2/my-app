4d79c041865318f0753f7abfa2ace107d991a11e
chandannaresh <chandansutradhar2@gmail.com>
2023-03-09 14:57:44 +0530

    fun4 added

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function fun1(){
     console.log('fun 1 invoked');
+    console.log('fun1 modified line');
 }
 
 function fun2(){
     console.log('fun2 invoked')
+}
+
+function fun3(){
+    console.log('fun3 invoked');
+}
+
+
+function fun4(){
+    console.log('fun4 invoked');
 }
